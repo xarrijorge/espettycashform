6b3ae667d13f1ea064f928afba5c435125adf803
xarrijorge <kodinglife@gmail.com>
2022-04-21 13:35:30 +0000

    Submission working

--- src/App.js
+++ src/App.js
@@ -5,19 +5,47 @@ import { BsFillPlusCircleFill } from 'react-icons/bs'
 import './App.css'
 
 function App() {
-    const [list, setList] = React.useState([<Item />])
+    const [list, setList] = React.useState([<Item count={0} />])
+    const [submitData, setSubmitData] = React.useState({})
 
     const updateList = (e) => {
         e.preventDefault()
         setList([...list, <Item />])
-        console.log(list)
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        localStorage.setItem('data', JSON.stringify(submitData))
+        alert(submitData)
+    }
+
+    React.useEffect(() => {}, [])
+
     return (
-        <form className='App'>
-            {list.map((item, key) => item)}
-            <BsFillPlusCircleFill className='addButton' onClick={updateList} />
-        </form>
+        <>
+            <h3 style={{ textAlign: 'center' }}>
+                Welcome USER. Please fill out the form below.
+            </h3>
+            <form className='App' onSubmit={handleSubmit}>
+                {list.map((item, index) => (
+                    <Item
+                        data={submitData}
+                        setData={setSubmitData}
+                        count={index}
+                        key={index}
+                    />
+                ))}
+                <button
+                    disabled={list.length >= 15 ? true : false}
+                    className='addButton'
+                    onClick={updateList}>
+                    <BsFillPlusCircleFill />
+                </button>
+                <button className='submitButton' type='submit'>
+                    submit
+                </button>
+            </form>
+        </>
     )
 }
 
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,47 +1,65 @@
 import React from 'react'
 import TextField from '@mui/material/TextField'
 
-const Item = ({ count }) => {
+const Item = ({ count, data, setData }) => {
     const [unitCost, setCost] = React.useState(1)
     const [amount, setAmount] = React.useState(1)
+    const [itemName, setItemName] = React.useState('')
     const [total, setTotal] = React.useState(0)
+    const [elData, setElData] = React.useState({})
 
     React.useEffect(() => {
-        setTotal(unitCost * amount)
-    }, [unitCost, amount])
+        setTotal(amount * unitCost)
+        setData({ ...data, [itemName]: { ...elData, total: total } })
+    }, [amount, itemName, data, elData, unitCost, total, setTotal, setData])
+
     return (
-        <>
-            <p>
-                <TextField
-                    required
-                    type='text'
-                    size='small'
-                    id='outlined-required'
-                    label='Item Name'
-                />
-                <TextField
-                    required
-                    type='number'
-                    size='small'
-                    label='Unit Cost'
-                    value={unitCost}
-                    onChange={(e) => setCost(e.target.value)}
-                />
-                <TextField
-                    type='number'
-                    size='small'
-                    label='Amount'
-                    value={amount}
-                    onChange={(e) => setAmount(e.target.value)}
-                />
-                <TextField
-                    type='number'
-                    size='small'
-                    label='Total'
-                    value={total}
-                />
-            </p>
-        </>
+        <div className='elDiv'>
+            <TextField
+                className='textInput'
+                required
+                type='text'
+                size='small'
+                id='outlined-required'
+                label='Item Name'
+                name={`item${count}`}
+                onChange={(e) => setItemName(e.target.value)}
+            />
+            <TextField
+                className='textInput'
+                required
+                type='number'
+                size='small'
+                label='Unit Cost'
+                onChange={(e) => {
+                    setCost(e.target.value)
+                    setElData({ ...elData, cost: e.target.value })
+                }}
+                name={`cost${count}`}
+            />
+            <TextField
+                type='number'
+                className='textInput'
+                size='small'
+                label='Amount'
+                onChange={(e) => {
+                    setAmount(e.target.value)
+                    setElData({
+                        ...elData,
+                        amount: e.target.value,
+                    })
+                }}
+                name={`amount${count}`}
+            />
+            <TextField
+                className='textInput'
+                type='number'
+                size='small'
+                label='Total'
+                value={total}
+                name={`total${count}`}
+            />
+        </div>
     )
 }
 
